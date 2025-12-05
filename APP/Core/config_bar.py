4ea608af274d965e687508ebc359a03b204abb25
LimaLucas21 <lucaslima96@outlook.com.br>
2025-12-05 10:00:40 -0300

    subindo alterações

--- APP/Core/config_bar.py
+++ APP/Core/config_bar.py
@@ -1,13 +1,25 @@
 from tkinter import filedialog, messagebox
 import tkinter as tk
+import os
 
 class ConfigBar:
     def __init__(self, app):
         self.app = app
 
+    def func_editar(self, operacao):
+        if operacao == "cut":
+            self.app.Texto.event_generate("<<Cut>>")
+        elif operacao == "copy":
+            self.app.Texto.event_generate("<<Copy>>")
+        elif operacao == "paste":
+            self.app.Texto.event_generate("<<Paste>>")
+        elif operacao == "select_all":
+            self.app.Texto.tag_add('sel', '1.0', 'end')
+
     def novo_arquivo(self):
         self.app.Texto.delete('1.0', tk.END)
         self.app.arquivo_atual = None   
+        self.app.janela.tittle('Bloco de Notas - Novo Arquivo')
 
     def abrir_arquivo(self):
         arquivo_selecionado = filedialog.askopenfilename()
@@ -19,12 +31,32 @@ class ConfigBar:
                 self.app.Texto.delete('1.0', tk.END)
                 self.app.Texto.insert('1.0', conteudo)
                 self.app.arquivo_atual = arquivo_selecionado
+                self.app.janela.title(f"Bloco de Notas - {os.path.basename(arquivo_selecionado)}")
 
             except Exception as e:
                 messagebox.showerror("Erro", f"Não foi possível abrir o arquivo: {e}")
         
     def salvar_arquivo(self):
-        if self.app.arquivo_atual:
+        arquivo_atual = self.app.arquivo_atual
+        if not arquivo_atual:
+            self.salvar_arquivo_como()
+        else:
+            try:
+                conteudo = self.app.Texto.get('1.0', tk.END)
+                with open(arquivo_atual, 'w', encoding='utf-8') as arquivo:
+                    arquivo.write(conteudo)
+            except Exception as e:
+                messagebox.showerror("Erro", f"Não foi possível salvar o arquivo: {e}")
 
-        else:    
-            self.app.arquivo_atual = 
+    def salvar_arquivo_como(self):
+        arquivo_selecionado = filedialog.asksaveasfilename(defaultextension='.txt',filetypes=[("Descrição", "*.extensão"), ("Outra descrição", "*.*")])
+        if arquivo_selecionado:
+            try:
+                conteudo = self.app.Texto.get('1.0', tk.END)
+                with open(arquivo_selecionado, 'w', encoding='utf-8') as arquivo:
+                    arquivo.write(conteudo)
+                    self.app.arquivo_atual = arquivo_selecionado
+                    self.app.janela.title(f"Bloco de Notas - {os.path.basename(arquivo_selecionado)}")
+
+            except Exception as e:
+                messagebox.showerror("Erro", f"Não foi possível salvar o arquivo: {e}")
--- APP/main.py
+++ APP/main.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import filedialog, messagebox
-from Core.config_bar import ConfigBar
+from Core.config_bar import ConfigBar 
 
 class BlocoNotas:
     def __init__(self):
@@ -9,19 +9,37 @@ class BlocoNotas:
         self.janela = tk.Tk()
         self.janela.title("Bloco de Notas")
         self.janela.geometry("800x600")
-        self.Texto = tk.Text(self.janela, wrap='word', undo=True)
-        self.Texto.pack(expand=True, fill='both')
+
+        #
+        self.frame_principal = tk.Frame(self.janela)
+        self.frame_
+        #
+
         self.Menu = tk.Menu(self.janela)
+
         self.janela.config(menu=self.Menu)
+
         self.MenuArquivo = tk.Menu(self.Menu, tearoff=0)
         self.MenuEditar = tk.Menu(self.Menu, tearoff=0)
+
         self.Menu.add_cascade(label="Arquivo", menu=self.MenuArquivo)
         self.Menu.add_cascade(label="Editar", menu=self.MenuEditar)
+
         self.MenuArquivo.add_command(label="Novo", command=self.config_bar.novo_arquivo)
         self.MenuArquivo.add_command(label="Abrir", command=lambda: self.config_bar.abrir_arquivo())
         self.MenuArquivo.add_command(label="Salvar", command=self.config_bar.salvar_arquivo)
         self.MenuArquivo.add_separator()
         self.MenuArquivo.add_command(label="Sair", command=self.janela.quit)
+
+
+        self.MenuEditar.add_command(label= 'Desfazer', command=self.Texto.edit_undo)
+        self.MenuEditar.add_command(label= 'Refazer', command=self.Texto.edit_redo)
+        self.MenuEditar.add_command(label='Recortar', command=lambda: self.config_bar.func_editar("cut"))
+        self.MenuEditar.add_command(label='Copiar', command=lambda: self.config_bar.func_editar("copy"))
+        self.MenuEditar.add_command(label='Colar', command=lambda: self.config_bar.func_editar("paste"))
+        self.MenuEditar.add_separator()
+        self.MenuEditar.add_command(label='Selecionar Tudo', command=lambda: self.config_bar.func_editar("select_all"))
+        
         # 2. Variável para armazenar caminho do arquivo atual
         self.arquivo_atual = None
 
